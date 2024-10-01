c71ab66a4f8ce68e29310b5197d74d4030963cdb
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-01 07:56:01 +0300

    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./core/templates/**/*.html"],
+  content: [
+    "./templates/**/*.html",
+    "./core/templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
