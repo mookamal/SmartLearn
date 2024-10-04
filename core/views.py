48e20e05f7ccbb6d2a1a7c2999e4ffe47fb1cd98
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-04 14:38:52 +0300

    add template for page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Page
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def home(request):
     pages = Page.objects.all()[:4]
     return render(request, 'core/home.html', {'pages': pages})
+
+
+def page(request, slug):
+    page = get_object_or_404(Page, slug=slug)
+    return render(request, 'core/page.html', {'page': page})
