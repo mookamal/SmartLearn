afa492b505312807d89092f7c46fc46074e8fc46
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-17 17:44:26 +0300

    add status question

--- exams/templatetags/exam_tags.py
+++ exams/templatetags/exam_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from exams.models import Exam, Source, Subject
+from exams.models import Exam, Source, Subject, Question, Session, Answer
 
 register = template.Library()
 
@@ -24,3 +24,24 @@ def question_count_by_subject(exam_id, subject_id):
         return questions_count
     except (Exam.DoesNotExist, Subject.DoesNotExist):
         return 0
+
+
+@register.filter(name="question_status")
+def question_status(session_id, question_id):
+    try:
+        session = Session.objects.filter(id=session_id).first()
+        question = Question.objects.filter(id=question_id).first()
+
+        if not session or not question:
+            return "not found"
+
+        answer = Answer.objects.filter(
+            question=question, session=session).first()
+
+        if answer:
+            return '<i class="fa-solid fa-square-check text-green-400"></i>' if answer.is_correct else '<i class="fa-solid fa-square-xmark text-red-400"></i>'
+        else:
+            return '<i class="fa-solid fa-circle-question text-yellow-200"></i>'
+
+    except Exception as e:
+        return str(e)
