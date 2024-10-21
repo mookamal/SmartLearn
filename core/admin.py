bd9e5bff12c0de89b4b97e1e84ba79400e5cbcc7
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-21 20:33:58 +0300

    add

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Info, Page
+from django.core.cache import cache
 # Register your models here.
-info_name = Info.objects.first().name if Info.objects.exists() else "Info"
+
+site_name = "Admin Ask Pro"
+
 
 admin.site.register(Page)
 admin.site.register(Info)
-admin.site.site_header = info_name
-admin.site.site_title = info_name
+admin.site.site_header = site_name
+admin.site.site_title = site_name
