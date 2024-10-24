1e90b291f75de208fe578e79b4c70ee2452fd648
mohamed kamal <mohamed.mohamed1996318@gmail.com>
2024-10-25 01:49:56 +0300

    finish notify system

--- notify/urls.py
+++ notify/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import read_all_notify
+from .views import read_all_notify, notify_list
 
 urlpatterns = [
+    path("notify_list/", notify_list, name="notify_list"),
+    # ajax
     path("ajax/read-all-notify/", read_all_notify, name="read_all_notify"),
 ]
--- notify/views.py
+++ notify/views.py
@@ -5,6 +5,15 @@ from django.http import JsonResponse
 from .models import Notify
 # Create your views here.
 
+# views
+
+
+@login_required
+def notify_list(request):
+    notifications = Notify.objects.filter(
+        user=request.user).order_by('-created_at')
+    return render(request, 'notify/notify_list.html', {'notification_all': notifications})
+
 # ajax actions
 
 
