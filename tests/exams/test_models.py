6f1190405d5fad6d0d4d67f4b0d1b70e4590898b
mohmaed Kamal <mohamed.mohamed1996318@gmail.com>
2025-08-04 01:59:53 +0300

    Add tests for category total questions and total exams

--- tests/exams/test_models.py
+++ tests/exams/test_models.py
@@ -6,3 +6,8 @@ def test_category_total_questions(db,category_factory, question_factory,exam_fac
     exam = exam_factory(category=category)
     question_factory.create_batch(5,exam=exam)
     assert category.total_questions() == 5
+
+def test_category_total_exams(db,category_factory, exam_factory):
+    category = category_factory()
+    exam_factory.create_batch(5,category=category,is_visible=True)
+    assert category.total_exams() == 5
